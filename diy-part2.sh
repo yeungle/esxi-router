45498acaf45a015cf266c1fa0628424d76fe7fd9
yeungle <75871729+yeungle@users.noreply.github.com>
2024-04-03 10:56:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,17 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/luci/themes/luci-theme-argon
+
+git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
+
+git clone --depth=1 https://github.com/kenzok8/small-package.git kenzok8-packages
+cp -rf kenzok8-packages/ddnsto package/ddnsto
+cp -rf kenzok8-packages/luci-app-ddnsto package/luci-app-ddnsto
+
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+
+./scripts/feeds update -a
+./scripts/feeds install -a -f
