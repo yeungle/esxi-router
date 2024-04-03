56eb60e2f8342c69771d057d36f6cc38160e4196
yeungle <75871729+yeungle@users.noreply.github.com>
2024-04-03 11:11:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -24,9 +24,10 @@ rm -rf feeds/luci/themes/luci-theme-argon
 
 git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
 
-git clone --depth=1 https://github.com/kenzok8/small-package.git kenzok8-packages
-cp -rf kenzok8-packages/ddnsto package/ddnsto
-cp -rf kenzok8-packages/luci-app-ddnsto package/luci-app-ddnsto
+git clone --depth=1 https://github.com/linkease/nas-packages.git nas-packages
+cp -rf nas-packages/network/services/ddnsto
+git clone --depth=1 https://github.com/linkease/nas-packages-luci.git nas-packages-luci
+cp -rf nas-packages-luci/luci/luci-app-ddnsto
 
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 
